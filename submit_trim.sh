0478f8facea0b81d9ca98d2b809376573637045c
Greg Deakin <GregDeakin@users.noreply.github.com>
2016-04-11 14:02:18 +0100

    Update submit_trim.sh

--- submit_trim.sh
+++ submit_trim.sh
@@ -5,10 +5,9 @@
 
 FORWARD=$1
 REVERSE=$2
-OUTDIR=$3
-TRIMLOC=$4
-QUALITY=$5
-MINLEN=$6
+TRIMLOC=$3
+QUALITY=$4
+MINLEN=$5
 
 java -jar $TRIMLOC/trimmomatic-0.33.jar PE -phred33 $1 $2 $1.trimmed.fq /dev/null $2.trimmed.fq /dev/null ILLUMINACLIP:$TRIMLOC/illumina_full_adapters.fa:2:30:10 SLIDINGWINDOW:8:$QUALITY MINLEN:$MINLEN
 
