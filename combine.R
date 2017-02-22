10f56e2a5194fdba3b5966b6963e0ddd48c8f09b
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-02-22 16:11:29 +0000

    Update combine.R

--- combine.R
+++ combine.R
@@ -10,12 +10,7 @@ lapply(qq,function(x) {names(x)[7]<-sub("\\/.*","",sub(".*\\/WTCHG","WTCHG",name
 #merge the list of data tables into a single data table
 m <- Reduce(function(...) merge(..., all = T,by=c("Geneid","Chr","Start","End","Strand","Length")), qq)
 	
-#convert data table to data frame for use with DESeq2
-countData <- data.frame(m[,c(1,7:(ncol(m))),with=F])
-rownames(countData) <- countData[,1] 
-countData <- countData[,-1]
-
-#output countData
-write.table(countData,"countData",sep="\t",na="",quote=F)
+#output "countData"
+write.table(m[,c(1,7:(ncol(m))),with=F],"countData",sep="\t",na="",quote=F) 
 #output gene details
 write.table(m[,1:6,with=F],"genes.txt",sep="\t",quote=F,row.names=F) 
