d35b2cc7f6bf207940f93205916da2be7fdf8029
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-05-05 14:18:50 +0100

    Add files via upload

---
+++ myfunctions/R/combine.R
@@ -0,0 +1,16 @@
+#install and load libraries		
+require(data.table)
+
+#load tables into a list of data tables
+qq <- lapply(list.files(".",".*.txt$",full.names=T,recursive=T),function(x) fread(x))
+
+#rename the sample columns 
+lapply(qq,function(x) {names(x)[7]<-sub("\\/.*","",sub(".*\\/WTCHG","WTCHG",names(x)[7]))})
+
+#merge the list of data tables into a single data table
+m <- Reduce(function(...) merge(..., all = T,by=c("Geneid","Chr","Start","End","Strand","Length")), qq)
+	
+#output "countData"
+write.table(m[,c(1,7:(ncol(m))),with=F],"countData",sep="\t",na="",quote=F) 
+#output gene details
+write.table(m[,1:6,with=F],"genes.txt",sep="\t",quote=F,row.names=F) 
