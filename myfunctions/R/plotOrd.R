6cfb3a50fbf2a36c10ee935d7bb28fdc7b4729d3
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-07-13 09:30:02 +0100

    Update plotOrd.R

--- myfunctions/R/plotOrd.R
+++ myfunctions/R/plotOrd.R
@@ -2,90 +2,101 @@
 plotOrd <- function (	
 	obj, 
 	colData,
-	design = "condition",
+	design,
 	shapes,
 	labels=F,
 	cluster=NULL,
-	continuous=F, 
+	continuous=F,
+	colourScale=c(low="#0072B2", high="#D55E00"),
+	cbPalette=T,
+	pointSize=2,
+	textSize=11,
+	textFont="Helvetica",
 	xlims=NULL,
 	ylims=NULL,
 	legend=T,
+	title=NULL,
 	xlabel="Dimension 1",
 	ylabel="Dimension 2",
 	dimx=1,
 	dimy=2,
-	pointSize=2,
-	cbPalette=F,
 	...
 ) {
-
 	suppressPackageStartupMessages(require(ggplot2))
-   
-	if (!all(design %in% names(colData))) {
-		stop("the argument 'design' should specify columns of colData")
-	}
-	design.df <- as.data.frame(colData[, design,drop = FALSE])
-	group <- if (length(design) > 1) {
-		factor(apply(design.df, 1, paste, collapse = " : "))
-	}
-	else {
-		if(continuous) {
-			colData[[design]]
-		} else {	
-			as.factor(colData[[design]])
+
+	d <- data.frame(PC1 = obj[, dimx], PC2 = obj[, dimy])
+	
+	if (!missing(design)) {
+		design.df <- as.data.frame(colData[, design,drop = FALSE])
+		group<- if (length(design) > 1) {
+			factor(apply(design.df, 1, paste, collapse = " : "))
 		}
+		else {
+			colData[[design]]
+		}
+		d <- cbind(d,design=design,design.df)
+		colnames(d)[grep("group", colnames(d))] <- design
 	}
 	
 	if (!missing(shapes)) {
-		shape <- as.factor(colData[[shapes]])
-		d <- data.frame(PC1 = obj[, dimx], PC2 = obj[, dimy], group = group,design.df,shape = shape)
-		colnames(d)[grep("shape", colnames(d))] <- shapes
-	} else {
-		d <- data.frame(PC1 = obj[, dimx], PC2 = obj[, dimy], group = group,design.df)
+		shape <- if (length(shapes) > 1) {
+			factor(apply(as.data.frame(colData[, shapes,drop = FALSE]), 1, paste, collapse = " : "))
+		} else {
+			as.factor(colData[[shapes]])
+		}
+		d <- cbind(d,shapes = shape)
 	}
-
-	colnames(d)[grep("group", colnames(d))] <- design
+	
+	if(!is.null(cluster)) {
+		km <- kmeans(obj,...)
+		d$Cluster<-km$cluster
+	}	
 
 	cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-	g <- ggplot()
+	g <- ggplot(data=d,aes(x=PC1, y=PC2))
 	g <- g + coord_fixed(ratio = 1, xlim = xlims, ylim = ylims, expand = TRUE)
-	g <- g + theme_bw()
-	g <- g + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-	g <- g + theme(axis.line.x = element_line(size=0.5,colour = "black"),axis.line.y = element_line(size=0.5,colour = "black"),axis.text = element_text(colour = "black"))
+
+	g <- g + theme_classic_thin(textSize,textFont)
+
 	if(!legend) {
-		g <- g+ theme(legend.position="none")
-	}	
-	if (!missing(shapes)) {
-		g <- g + geom_point(data=d, mapping=aes(x=PC1, y=PC2, colour=group, shape=shape),size=pointSize)
-		g <- g + scale_shape_discrete(name=shapes)
+		g <- g + theme_classic(textSize,textFont) %+replace% theme(legend.position="none")
 	} else {
-		g <- g + geom_point(data=d, mapping=aes(x=PC1, y=PC2, colour=group),size=pointSize)
+		g <- g + theme_classic(textSize,textFont)
 	}
+
+	if(!missing(design)) {
+		g <- g + aes(colour=group)
+	}
+	
+	if (!missing(shapes)) {
+		g <- g + aes(shape=shapes)
+		g <- g + scale_shape_discrete(name=shapes)
+	}
+	
+	g <- g + geom_point(size=pointSize)
+	
 	if(labels) {
-		g <- g + geom_text(data=d, mapping=aes(x=PC1, y=PC2, label=row.names(obj),colour=group), size=(pointSize+1), vjust=2, hjust=0.5)
+		g <- g + aes(label=row.names(obj))
+		g <- g + geom_text(size=(pointSize+1), vjust=2, hjust=0.5)
 	}
-
-
+	
 	if(continuous) {
-		#g <- g + scale_color_gradientn(colours = rainbow(5))
-		g <- g + scale_color_gradient(low="#edf8b1", high="#2c7fb8",name=design)
+		g <- g + scale_color_gradient(low=colourScale[1], high=colourScale[2],name=design)
 	} else {
 		if(cbPalette) {
 			g<-g+scale_colour_manual(values=cbbPalette)			
-		} else {	
-			g <- g + scale_colour_discrete(name=design)
+		} else {
+			g<-g+scale_colour_manual(colours=rainbow())
 		}
-
-		if(!is.null(cluster)) {
-			km <- kmeans(obj,...)
-			d$Cluster<-km$cluster
-			g<-g+stat_ellipse(data=d,mapping=aes(x=PC1,y=PC2,fill=factor(Cluster)), geom="polygon", level=cluster, alpha=0.2)
-		}
-	
 	}
+	
+	if(!is.null(cluster)) {
+		g<-g+stat_ellipse(geom="polygon", level=cluster, alpha=0.2)
+	}
+	
 	g <- g + xlab(xlabel)
 	g <- g + ylab(ylabel)
+	
 	return(g)
 }
-
