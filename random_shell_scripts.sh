09a20812ef42af146f35f17f9cd893fc829354ce
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-10-09 11:39:04 +0100

    Update random_shell_scripts.sh

--- random_shell_scripts.sh
+++ random_shell_scripts.sh
@@ -5,6 +5,11 @@ awk '{if(match($0,/^>/)){if(length(SEQ)>0){print HEADER"\n"SEQ};HEADER=$0;SEQ=""
 awk '{if(!gsub(/">"/,"")) {$0=toupper($0);COUNT=COUNT+gsub(/G|C/,"")}} END {print COUNT}' input.fasta
 awk '{if(!gsub(/">"/,"")) {$0=toupper($0);COUNT=COUNT+gsub(/A|T/,"")}} END {print COUNT}' input.fasta
 awk '{if(!gsub(/">"/,"")) {$0=toupper($0);COUNT=COUNT+gsub(/[^ATCG]/,"")}} END {print COUNT}' input.fasta
-
 # here is the single command version (if you want to count ambigueos bases, write something yourself (actually I might do this as it would be fun))
 awk '{if(!gsub(/">"/,"")) {$0=toupper($0);COUNT_GC=COUNT_GC+gsub(/G|C/,"");COUNT_AT=COUNT_AT+gsub(/A|T/,"");COUNT_ELSE=COUNT_ELSE+gsub(/[^ATCG]/,"")}} END {print "GC:\t"COUNT_GC"\nAT:\t"COUNT_AT"\nOthers:\t"COUNT_ELSE}' input.fasta
+
+### extract sequences from a fasta file based on a list of unformated headers (use the above script to convert the fasta file to single line)
+# This is a bit of a hack which will search through the fasta file for every header in your list - if either the fasta file or list are long use a different method
+# -P(perl) -o(only matching) grep for a partial header in a text file. Second part returns matches to the header +1 line
+grep -Po "FGSG_\d*" a_list.txt|xargs -I HEADER grep -A 1 HEADER singled.fasta > a_list.fa
+
